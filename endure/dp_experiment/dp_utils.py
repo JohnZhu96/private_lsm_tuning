7b66b9e41ba6b2556a463f15e0ba760fc2ee0803
Linfeng Zhu <linfengzhu@brandeis.edu>
2025-04-30 17:47:56 -0400

    sensitivity = 2

--- endure/dp_experiment/dp_utils.py
+++ endure/dp_experiment/dp_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from endure.lsm import Workload
 
-def perturb_workload2(workload: Workload, epsilon: float, sensitivity: float = 1.0) -> Workload:
+def perturb_workload(workload: Workload, epsilon: float, sensitivity: float = 2.0) -> Workload:
     scale = sensitivity / epsilon
     noisy_values = np.array([workload.z0, workload.z1, workload.q, workload.w]) + \
                    np.random.laplace(0, scale, 4)
@@ -13,7 +13,7 @@ def perturb_workload2(workload: Workload, epsilon: float, sensitivity: float = 1
     return Workload(z0=noisy_values[0], z1=noisy_values[1], q=noisy_values[2], w=noisy_values[3])
 
 
-def perturb_workload(workload: Workload, epsilon: float, sensitivity: float = 2.0, seed: int = None) -> Workload:
+def perturb_workload2(workload: Workload, epsilon: float, sensitivity: float = 2.0, seed: int = None) -> Workload:
     # set random seed for reproducibility
     if seed is not None:
         np.random.seed(seed)
